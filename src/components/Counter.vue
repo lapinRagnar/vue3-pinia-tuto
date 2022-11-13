00ef6226d5a007abf69f9b1fd805375cd884b7db
lapinRagnar <lapinragnar@gmail.com>
2022-11-13 11:46:56 +0100

    2 - declarer les states count et modifier le state avec getter, ici c'est la fonction increment et decrement et afficher dans Home

---
+++ src/components/Counter.vue
@@ -0,0 +1,24 @@
+<template>
+  
+  <h1>Counter</h1>
+  <h3>Afficher les données de mon useCounterStore </h3>
+
+  <h3> Counter : {{ store.count }}</h3>
+  
+  <button @click="store.increment">+</button>
+  <button @click="store.decrement">-</button>
+  <h3> Modifier la valeur de counter dans un computed
+     - Double Counter : {{ store.doubleCount }}
+  </h3>
+
+
+</template>
+
+<script setup>
+
+  import { useCounterStore } from '@/stores/counter.js'
+  
+  const store = useCounterStore()
+
+
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,12 @@
 <script setup>
-  import { useCounterStore } from '@/stores/counter.js'
 
-  const store = useCounterStore()
+  import Counter from '@/components/Counter.vue'
 
-  console.log(store)
 </script>
 
 <template>
   <main>
     <h1>Home Page</h1>
+    <Counter />
   </main>
 </template>
